c62732d807c4d3068be036c9c5f5b79e65b5ef4e
Waseem <waseem07799@gmail.com>
2019-12-31 11:30:44 +0530

    fixed api route and personality loading

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ import chrsma as ch
 
 app = Flask(__name__)
 
-
-male_personality = []
-female_personality = []
+male_personality = ch.loadData("male")
+female_personality = ch.loadData("female")
 
 @app.route('/')
 def server_check():
@@ -19,12 +18,11 @@ def errorResponse(message):
                 "message":message
             })
 
-@app.route('/api/getchrmsa')
+@app.route('/api/getchrsma')
 def chrsma():
     name = str(request.args.get('name'))
     gender = str(request.args.get('gender'))
     age = str(request.args.get('age'))
-    print(name,gender,age)
     if name == 'None' or name.strip() == "":
         return errorResponse("invalid name")
     if gender == 'None' or gender.strip() == "" or not (gender.strip() == "male" or gender.strip() == "female"):
@@ -42,8 +40,6 @@ def chrsma():
 
 
 if __name__ == '__main__':
-    male_personality = ch.loadData("male")
-    female_personality = ch.loadData("female")
     app.run()
 
 
