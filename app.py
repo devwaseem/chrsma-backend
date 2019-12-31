3a1f15ddc805e081f919cc417e5f0908a85e7656
Waseem <waseem07799@gmail.com>
2019-12-31 11:18:32 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, redirect, url_for, request, abort,Response,json
+import chrsma as ch
+
+app = Flask(__name__)
+
+
+male_personality = []
+female_personality = []
+
+@app.route('/')
+def server_check():
+   return "SERVER CHECK OK"
+
+
+def errorResponse(message):
+        return json.dumps({
+                "success":False,
+                "data":None,
+                "message":message
+            })
+
+@app.route('/api/getchrmsa')
+def chrsma():
+    name = str(request.args.get('name'))
+    gender = str(request.args.get('gender'))
+    age = str(request.args.get('age'))
+    print(name,gender,age)
+    if name == 'None' or name.strip() == "":
+        return errorResponse("invalid name")
+    if gender == 'None' or gender.strip() == "" or not (gender.strip() == "male" or gender.strip() == "female"):
+        return errorResponse("invalid gender")
+    if age == 'None' or age.strip() == "" or (not age.isnumeric()) or (int(age) < 5 or int(age) > 100):
+        return errorResponse("invalid age")
+    #all test passed
+    reponseStr = ch.generatePersonality(name,gender,age,personalities=male_personality if gender=="male" else female_personality)
+    return json.dumps({
+            "success":True,
+            "data":reponseStr,
+        })
+        
+
+
+
+if __name__ == '__main__':
+    male_personality = ch.loadData("male")
+    female_personality = ch.loadData("female")
+    app.run()
+
+
+
---
+++ chrsma.py
@@ -0,0 +1,36 @@
+import hashlib
+
+def loadData(gender):
+    personalities = []
+    filename = "male_personality.data" if gender == "male" else "female_personality.data"
+    with open(filename,"r") as f:
+        data = f.read()
+        data = data.splitlines()
+        sets = int(len(data)/5)
+        for i in range(5):
+            personalities.append(data[0:sets])
+            data = data[sets:]
+        return personalities
+
+
+def generatePersonality(name,gender,age,personalities):
+    lower_name = str(name).lower()
+    superkey = f"{lower_name}-{gender}-{age}" #sanjay-male-20
+    hashstr = hashlib.sha1(str.encode(str(superkey))).hexdigest() 
+    hashstrarr = [x for x in hashstr] 
+    hashkeys = list(filter(lambda x:x.isnumeric(),hashstr)) 
+    hashkeys = hashkeys[:5] #33277
+    if len(hashkeys) < 5: #may never be called... probability : 1/1000000... just handling errors here
+        left = (5-len(hashkeys))
+        for i in range(left):
+            hashkeys.append(0)
+    hashkeys = [int(x) - 1 for x in hashkeys] #22166
+    personalityStr = ""
+    for i,e in enumerate(hashkeys):
+        personalityStr += personalities[i][e]
+        if not i == len(hashkeys)-1:
+            personalityStr = personalityStr.replace(".",", ")
+    replacement = {"him":name,"He":name}
+    for k,v in replacement.items():
+        personalityStr = personalityStr.replace(k,v)
+    return personalityStr
